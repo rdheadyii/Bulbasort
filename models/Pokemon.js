9770bb7b2429a260bff866e0118f0061f930eff9
Kona <landon.deroin@gmail.com>
2023-08-11 11:28:03 -0400

    added pokemon model

--- models/Pokemon.js
+++ models/Pokemon.js
@@ -1,4 +1,32 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 class Pokemon extends Model {}
+
+Pokemon.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  gameExclusive: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    reference: {
+        model: 'game',
+        key: 'game'
+    },
+  },
+  
+},
+    {
+    sequelize,
+    modelName: 'pokemon';
+    }
+);
+module.exports = Pokemon;
