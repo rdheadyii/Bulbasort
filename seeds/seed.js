03e31af6128ba311b46bd7c039838820e53a8967
rdheadyii <rdheadyii@gmail.com>
2023-08-13 13:10:06 -0400

    wrote more of seed js and started changing svpokedex json to match model

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,3 +7,42 @@ const teamData = require('./teamData.json');
 const genData = require('./genData.json');
 const svPokedex = require('./svpokedex.json');
 const pokeTeamData = require('./pokeTeamData.json');
+
+const seedDatabase = async () => {
+    // sync sequelize
+    await sequelize.sync({ force: true });
+
+    // seed user data
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    // seed game data
+    await Game.bulkCreate(gameData, {
+        returning: true,
+    });
+
+    // seed team data
+    for (const team of teamData) {
+        await Team.create({
+            ...team,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+        });
+    }
+
+    // seed generation data
+    await Generation.bulkCreate(genData, {
+        returning: true,
+    });
+
+    // seed pokedex data
+    await Pokemon.bulkCreate(svPokedex, {
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+// call the function
+seedDatabase();
