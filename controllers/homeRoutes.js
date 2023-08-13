2652baabcf5a1b67657003a765d968cddbe3683f
Travis <travis3214@yahoo.com>
2023-08-13 18:28:25 -0400

    added a get for games on homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Game, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Made a get for all games
+router.get('/', async (req, res) => {
+    try {
+      
+      const gameData = await Game.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const Games = gameData.map((Game) => Game.get({ plain: true }));
+      res.render('homepage', { 
+        Games, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
