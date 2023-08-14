79248bf36bad0214462896dd0ff746889f5a9413
Travis <travis3214@yahoo.com>
2023-08-14 13:27:58 -0400

    added js for sign up

--- public/js/login.js
+++ public/js/login.js
@@ -22,6 +22,28 @@ const loginFormHandler = async (event) => {
     }
   };
   
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#name-signup').value.trim();
+    const userName = document.querySelector('#userName-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (name && userName && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, userName, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
   document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
