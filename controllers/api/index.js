3f89ac21c8c067d8c962b0425f7a4fa2c1e87b7e
Kona <landon.deroin@gmail.com>
2023-08-14 10:15:04 -0400

    added indexjs for api

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const gameRoutes = require('./gameRoutes.js');
+const pokemonRoutes = require('./pokemonRoutes.js');
+const teamRoutes = require('./teamRoutes.js');
+const userRoutes = require('./userRoutes.js');
+
+router.use('/games', gameRoutes);
+router.use('/pokemon', pokemonRoutes);
+router.use('/teams', teamRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
