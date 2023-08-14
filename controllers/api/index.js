84c71d15327a8fe6042a1cc204e080db89cbf304
rdheadyii <rdheadyii@gmail.com>
2023-08-14 19:50:07 -0400

    fixed genRoutes/gameRoutes/added genRoutes to index js in api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const gameRoutes = require('./gameRoutes.js');
 const pokemonRoutes = require('./pokemonRoutes.js');
 const teamRoutes = require('./teamRoutes.js');
 const userRoutes = require('./userRoutes.js');
+const genRoutes = require('./genRoutes.js')
 
 router.use('/games', gameRoutes);
 router.use('/pokemon', pokemonRoutes);
 router.use('/teams', teamRoutes);
 router.use('/users', userRoutes);
-router.use('/poketeam', PokeTeam)
+router.use('/poketeam', PokeTeam);
+router.use('/generation', genRoutes);
 
 module.exports = router;
