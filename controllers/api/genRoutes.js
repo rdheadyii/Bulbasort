6deae52384d3f0a13c1555e0713989e3b6916a58
rdheadyii <rdheadyii@gmail.com>
2023-08-15 18:44:08 -0400

    added render for get generation by id

--- controllers/api/genRoutes.js
+++ controllers/api/genRoutes.js
@@ -5,8 +5,7 @@ router.get('/', async (req, res) => {
     try{
         const generation = await Generation.findAll();
         res.status(200).json(generation);
-    }
-    catch (err) {
+    } catch (err) {
         console.error('failed to retreive pokemon', err);
         res.status(500).json(err)
     }
@@ -14,10 +13,16 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try{
-        const generation = await Generation.findByPk(req.params.id);
-        res.status(200).json(generation);
-    }
-    catch (err) {
+        const genData = await Generation.findByPk(req.params.id);
+
+        const generation = genData.get({plian: true});
+
+        res.render('generation', {
+            ...generation,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (err) {
         console.error('failed to retreive pokemon', err);
         res.status(500).json(err)
     }
