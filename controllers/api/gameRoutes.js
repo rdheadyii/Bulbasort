a2efd794ec2d5017977a8e7fd0afa45a3929546a
rdheadyii <rdheadyii@gmail.com>
2023-08-15 17:53:36 -0400

    updated gameRoutes to have render

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -2,23 +2,36 @@ const router = require('express').Router();
 const { Game } = require('../../models');
 
 router.get('/', async (req, res) => {
-    try{
-        const games = await Game.findAll();
-        res.status(200).json(games);
+    try {
+        const gameData = await Game.findAll();
+
+        const games = gameData.map((game) => game.get({ plain: true }));
+
+        res.render('game', {
+            games,
+            logged_in: req.session.logged_in
+        });
+
     }
     catch (err) {
-        console.error('failed to retreive pokemon', err);
+        console.error('Failed to retreive games', err);
         res.status(500).json(err)
     }
 });
 
 router.get('/:id', async (req, res) => {
-    try{
-        const games = await Game.findByPk(req.params.id);
-        res.status(200).json(games);
+    try {
+        const gameData = await Game.findByPk(req.params.id);
+
+        const game = gameData.get({plain: true});
+
+        res.render('game', {
+            ...game,
+            logged_in: req.session.logged_in
+        });
     }
     catch (err) {
-        console.error('failed to retreive pokemon', err);
+        console.error('Failed to retreive game', err);
         res.status(500).json(err)
     }
 });
