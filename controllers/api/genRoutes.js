c1ecb4a780b9784e79a5978027a4ed61f019ae36
rdheadyii <rdheadyii@gmail.com>
2023-08-15 18:57:26 -0400

    added render to gen routes get all

--- controllers/api/genRoutes.js
+++ controllers/api/genRoutes.js
@@ -3,8 +3,15 @@ const { Generation } = require('../../models');
 
 router.get('/', async (req, res) => {
     try{
-        const generation = await Generation.findAll();
-        res.status(200).json(generation);
+        const genData = await Generation.findAll();
+
+        const generations = genData.map((generation) => generation.get({plain: true}));
+
+        res.render('generation', {
+            generations,
+            logged_in: req.session.logged_in
+        });
+
     } catch (err) {
         console.error('failed to retreive pokemon', err);
         res.status(500).json(err)
@@ -15,7 +22,7 @@ router.get('/:id', async (req, res) => {
     try{
         const genData = await Generation.findByPk(req.params.id);
 
-        const generation = genData.get({plian: true});
+        const generation = genData.get({plain: true});
 
         res.render('generation', {
             ...generation,
